02a63c72d6c528b3c429b84202bf1e7d2e13378a
AD <ad@ADs-Mac-mini.local>
2026-06-29 13:51:17 +0100

    fix homepage routing

--- server.js
+++ server.js
@@ -243,7 +243,7 @@ app.post("/upload-contacts", upload.single("file"), (req, res) => {
 
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/upload.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(3000, () => {
